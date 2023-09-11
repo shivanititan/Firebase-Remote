059410a2f1babd8726851310a250ba2693be6f03
shivanititan <shivanivaikar@titan.co.in>
2023-09-11 11:17:21 +0530

    Remote Publisher

---
+++ Firebase-Remote/RemoteConfigPublisher/src/main/java/com/titan/ChangeLogManager.java
@@ -0,0 +1,75 @@
+package com.titan;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChangeLogManager {
+
+    public List<ChangeLogs> getFilesChanged() throws IOException {
+        File file = new File("/src/main/resources/ChangedFiles.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        ArrayList<String> list = new ArrayList<String>();
+        String st;
+        while ((st = br.readLine()) != null) {
+            list.add(st);
+        }
+        ArrayList<ChangeLogs> changeLogs = new ArrayList<ChangeLogs>();
+        for (String item : list) {
+            String[] split = item.split("\t");
+            if (split.length > 1) {
+                ChangeLogStatus status = null;
+                switch (split[0]) {
+                    case "M": {
+                        status = ChangeLogStatus.MODIFIED;
+                        break;
+                    }
+                    case "A": {
+                        status = ChangeLogStatus.NEW;
+                        break;
+                    }
+                    case "D": {
+                        status = ChangeLogStatus.DELETED;
+                        break;
+                    }
+                }
+                if (status != null) {
+                    changeLogs.add(new ChangeLogs(split[1], status));
+                }
+            }
+        }
+        return changeLogs;
+    }
+}
+
+class ChangeLogs {
+    private String fileName;
+    private ChangeLogStatus status;
+
+    public ChangeLogs(String fileName, ChangeLogStatus status) {
+        this.fileName = fileName;
+        this.status = status;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public ChangeLogStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeLogs{" +
+                "fileName='" + fileName + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
+
+enum ChangeLogStatus {
+    NEW,
+    MODIFIED,
+    DELETED
+}
